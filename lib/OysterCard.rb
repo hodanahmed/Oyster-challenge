b92456c52e5946f5820d5bfd5da72e6126e9eb3c
Hodan <hodanahmed0000@gmail.com>
2020-02-24 15:06:26 +0000

    User Story 2: Created failing rspec test. top_up undefined method:

--- lib/OysterCard.rb
+++ lib/OysterCard.rb
@@ -1,6 +1,8 @@
 class Oystercard
+#We can access the balance by calling subject.balance
   attr_reader :balance
 
+#When creating a oystercard instance, the balance will be 0.
 def initialize(balance = 0)
   @balance = balance
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,8 +1,13 @@
 require 'OysterCard'
 describe Oystercard do
 
-#Created a failing test for User Story 1: As a customer I want a balance on my card.
+#Created a passing test for User Story 1: As a customer I want a balance on my card.
 it 'can have a balance when initialized' do
   expect(subject.balance).to eq(0)
+end
+#Created a failing test for User Story 2: As I want to add money to my card.
+it 'can add money to the balance' do
+    subject.top_up(30)
+  expect(subject.balance).to eq(30)
 end
 end
