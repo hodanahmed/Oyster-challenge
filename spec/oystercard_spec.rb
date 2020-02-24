278818126a92fffb3b478e435b9cf726a8df78a2
Hodan <hodanahmed0000@gmail.com>
2020-02-24 14:57:00 +0000

    User Story 1: Created a failing rspec test

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,4 +1,8 @@
 require 'OysterCard'
 describe Oystercard do
 
+#Created a failing test for User Story 1: As a customer I want a balance on my card.
+it 'can have a balance when initialized' do
+  expect(subject.balance).to eq(0)
+end
 end
